f290981c46d757e2daa7d77c06911e503331d03a
Ethan <ethanjudahw@gmail.com>
2025-09-04 21:35:52 -0400

    I NORMALIZED THE VECTORS

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,36 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
+const SPEED = 7500.0
 #const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var xdirection := Input.get_axis("ui_left", "ui_right")
-	var ydirection := Input.get_axis("ui_up", "ui_down")
-#spacebar=ui_select
-	if xdirection:
-		velocity.x = xdirection * SPEED
+	## Add the gravity.
+	##if not is_on_floor():
+	##	velocity += get_gravity() * delta
+#
+	## Handle jump.
+	##if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	##	velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var xdirection := Input.get_axis("movement_left", "movement_right")
+	#var ydirection := Input.get_axis("movement_up", "movement_down")
+##spacebar=movement_select
+	#if xdirection:
+		#velocity.x = xdirection * SPEED * delta
+	#else:saaaaa
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#if ydirection:
+		#velocity.y = ydirection * SPEED * delta
+	#else:
+		#velocity.y = move_toward(velocity.y, 0, SPEED)
+	var direction := Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
+	if direction:
+		velocity = direction * SPEED * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	if ydirection:
-		velocity.y = ydirection * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity = velocity.normalized()
 	move_and_slide()
